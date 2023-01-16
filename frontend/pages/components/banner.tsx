9b90c587dd97da71f50cafb3728df73b649f92f4
Robert Williams <robert.williams@jisc.ac.uk>
2023-01-16 11:40:36 +0000

    amend validation and some css

--- frontend/pages/components/banner.tsx
+++ frontend/pages/components/banner.tsx
@@ -2,8 +2,7 @@ export const Banner = () => {
   return (
   <div className="flex-col bg-violet-600 text-center rounded-lg shadow-2xl p-4 mb-6 mt-10">
     <p className="">
-      <b>Try it free 7 days</b> then
-      <br /> $20/mo. thereafter
+      <b>Try it free 7 days</b> then $20/mo. thereafter
     </p>
   </div>
   );
--- frontend/pages/components/form.tsx
+++ frontend/pages/components/form.tsx
@@ -20,95 +20,103 @@ export const Form = () => {
   const { errors } = formState as any;
 
   function onSubmit(data: any) {
-    alert("SUCCESS!! :-)\n\n" + JSON.stringify(data, null, 4));
+    alert( "Thanks for signing up! You will receive an email shortly.");
     return false;
   }
 
   return (
     <>
-    <div className="w-4/5 mx-10">
-      <Banner />
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="bg-white shadow-md rounded-lg px-8 pt-14 pb-8 mb-4 mt-4"
-      >
-        <div className="mb-4">
-          <div className="relative">
-            <label className="sr-only" htmlFor="firstname">
+      <div className="px-10 sm:px-20">
+        <Banner />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-white shadow-md rounded-lg px-8 pt-14 pb-8 mb-4 mt-4"
+        >
+          <div className="mb-4">
+            <div className="relative">
+              <label className="sr-only" htmlFor="firstname">
+                First Name
+              </label>
+              <input
+                className={`shadow appearance-none border rounded w-full py-2 px-3 
+                          font-bold text-gray-700 leading-tight focus:outline-none focus:shadow-outline
+                           ${errors.firstName ? "is-invalid" : ""}`}
+                id="firstname"
+                type="text"
+                {...register("firstName")}
+                placeholder={errors.firstName ? "" : "First Name"}
+              />
+              <div className="text-red-500 text-xs italic text-right font-bold">
+                {errors.firstName?.message}
+              </div>
+            </div>
+          </div>
+          <div className="relative mb-4">
+            <label className="sr-only" htmlFor="lastname">
               First Name
             </label>
             <input
               className={`shadow appearance-none border rounded w-full py-2 px-3 
-                          font-bold text-gray-700 leading-tight focus:outline-none focus:shadow-outline
-                           ${errors.firstName ? "is-invalid" : ""}`}
-              id="firstname"
-              type="text"
-              {...register("firstName")}
-              placeholder="First Name"
-            />
-            <div className="text-red-400 text-xs italic">
-              {errors.firstName?.message}
-            </div>
-          </div>
-        </div>
-        <div className="relative mb-4">
-          <label className="sr-only" htmlFor="lastname">
-            First Name
-          </label>
-          <input
-            className={`shadow appearance-none border rounded w-full py-2 px-3 
               font-bold text-gray-700 leading-tight focus:outline-none focus:shadow-outline
                ${errors.lastName ? "is-invalid" : ""}`}
-            id="lastname"
-            type="text"
-            placeholder="Last Name"
-          />
-          <div className="text-red-400 text-xs">{errors.lastName?.message}</div>
-        </div>
-        <div className="relative mb-4">
-          <label className="sr-only" htmlFor="emailaddress">
-            Email Address
-          </label>
-          <input
-            className={`shadow appearance-none border rounded w-full py-2 px-3 
+              id="lastname"
+              type="text"
+              {...register("lastName")}
+              placeholder={errors.lastName ? "" : "Last Name"}
+            />
+            <div className="text-red-500 text-xs italic text-right font-bold">
+              {errors.lastName?.message}
+            </div>
+          </div>
+          <div className="relative mb-4">
+            <label className="sr-only" htmlFor="emailaddress">
+              Email Address
+            </label>
+            <input
+              className={`shadow appearance-none border rounded w-full py-2 px-3 
               font-bold text-gray-700 leading-tight focus:outline-none focus:shadow-outline
                ${errors.email ? "is-invalid" : ""}`}
-            id="emailaddress"
-            type="text"
-            placeholder="Email Address"
-          />
-          <div className="text-red-400 text-xs">{errors.email?.message}</div>
-        </div>
-        <div className="mb-6 relative">
-          <label className="sr-only" htmlFor="password">
-            Password
-          </label>
-          <input
-            className={`shadow appearance-none border rounded w-full py-2 px-3 
+              id="emailaddress"
+              type="text"
+              {...register("email")}
+              placeholder={errors.email ? "email@example/com" : "Email Address"}
+            />
+            <div className="text-red-500 text-xs italic text-right font-bold">
+              {errors.email?.message}
+            </div>
+          </div>
+          <div className="mb-6 relative">
+            <label className="sr-only" htmlFor="password">
+              Password
+            </label>
+            <input
+              className={`shadow appearance-none border rounded w-full py-2 px-3 
               font-bold text-gray-700 leading-tight focus:outline-none focus:shadow-outline
                ${errors.password ? "is-invalid" : ""}`}
-            id="password"
-            type="password"
-            {...register("password")}
-            placeholder="Password"
-          />
-          <div className="text-red-400 text-xs">{errors.password?.message}</div>
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-green-400 hover:bg-blue-700 text-white font-bold p-4 w-full rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            CLAIM YOUR FREE TRIAL
-          </button>
-        </div>
-        <p className="text-sm mb-4 text-center pt-4 text-gray-400">
-          By clicking the button, you are agreesing to our{" "}
-          <a href="#" className="font-bold text-red-700">
-            Terms and Services
-          </a>
-        </p>
-      </form>
+              id="password"
+              type="password"
+              {...register("password")}
+              placeholder={errors.password ? "" : "Password"}
+            />
+            <div className="text-red-500 text-xs italic text-right font-bold">
+              {errors.password?.message}
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-green-400 hover:bg-green-300 text-white text-sm font-bold p-4 w-full rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              CLAIM YOUR FREE TRIAL
+            </button>
+          </div>
+          <p className="text-sm mb-4 text-center pt-4 text-gray-400">
+            By clicking the button, you are agreesing to our{" "}
+            <a href="#" className="font-bold text-red-700">
+              Terms and Services
+            </a>
+          </p>
+        </form>
       </div>
     </>
   );
