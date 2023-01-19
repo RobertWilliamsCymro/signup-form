f333c75fc53f1fd4fdf0d038f8a82140fda9a290
Robert Williams <robert.williams@jisc.ac.uk>
2023-01-19 09:56:11 +0000

    removed unnecessary divs

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,11 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-        <title>Sign up</title>
-        <main>
-          <div className="flex justify-center items-center min-h-screen text-[16px] font-poppins text-white bg-red-300 bg-cover bg-[url(../public/bg-intro-mobile.png)] sm:bg-[url(../public/bg-intro-desktop.png)]">
-            <div className="flex flex-col sm:flex-row w-full">
-              <Heading />
-              <SignUpForm />
-            </div>
-          </div>
-        </main>
+      <title>Sign up</title>
+      <main className="flex flex-col sm:flex-row w-full justify-center items-center min-h-screen text-[16px] font-poppins text-white bg-red-400 bg-cover bg-[url(../public/bg-intro-mobile.png)] sm:bg-[url(../public/bg-intro-desktop.png)]">
+        <Heading />
+        <SignUpForm />
+      </main>
     </>
   );
 };
